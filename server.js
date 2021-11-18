512aa5277edd3e3c7a0ce73e050de15a055c2a9d
atharva <atharva.ek@gmail.com>
2021-11-18 09:10:03 +0530

    basic file setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+app.use(express.json());
+//app.use(express.static(path.join(__dirname, "/app")))
+const port = 5000;
+app.listen(process.env.PORT || port, () => {
+  console.log("server started.");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/app", "/index.html"), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
