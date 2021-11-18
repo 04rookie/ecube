6822b3a671865d4e4397667297f7f28cc2452ed1
atharva <atharva.ek@gmail.com>
2021-11-18 14:36:23 +0530

    added testimonials

--- client/src/component/Home/Testimonials.js
+++ client/src/component/Home/Testimonials.js
@@ -1,5 +1,87 @@
-import React from "react";
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+import proto from "./imageproto.png"
+export default function Testimonials() {
+  const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-export default function Testimonials(){
-    return <></>
+  const images = [
+    {
+      label: "1",
+      imgPath:
+        proto,
+    },
+    {
+      label: "2",
+      imgPath:
+        proto,
+    },
+    {
+      label: "3",
+      imgPath:
+        proto,
+    },
+    {
+      label: "4",
+      imgPath:
+        proto,
+    },
+  ];
+
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <><div style={{padding:"5vw 3vw 5vw", display:"flex", justifyContent:"center", backgroundColor:"#0F4C75"}}>
+      <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+        <Paper
+          square
+          elevation={0}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            height: 50,
+            pl: 2,
+            bgcolor: "#0F4C75",
+          }}
+        >
+          <Typography>{images[activeStep].label}</Typography>
+        </Paper>
+        <AutoPlaySwipeableViews
+          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
+          {images.map((step, index) => (
+            <div key={step.label}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <Box
+                  component="img"
+                  sx={{
+                    height: 255,
+                    display: "block",
+                    maxWidth: 400,
+                    overflow: "hidden",
+                    width: "100%",
+                  }}
+                  src={step.imgPath}
+                  alt={step.label}
+                />
+              ) : null}
+            </div>
+          ))}
+        </AutoPlaySwipeableViews>
+      </Box>
+      </div>
+    </>
+  );
 }
