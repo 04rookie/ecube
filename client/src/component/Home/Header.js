870a6eb3fed8dce3c8892b70ce7285af6072cc7e
atharva <atharva.ek@gmail.com>
2021-11-18 17:04:58 +0530

    added feature-home in a grid layout

--- client/src/component/Home/Header.js
+++ client/src/component/Home/Header.js
@@ -1,12 +1,39 @@
 import React from "react";
-import { Box } from "@mui/material";
-import man from "./header-man.png"
+import { Box, Grid } from "@mui/material";
+import man from "./header-man.png";
 export default function Header() {
   return (
     <div name="home-header-main">
-      <Box sx={{padding:"3vw", height:"26.9vw", backgroundColor:"#BBE1FA"}}>
-        <p style={{ fontSize:"3vw", fontFamily: "Montserrat", fontWeight: 700, color:"#1B262C" }}>E-cube</p>
-        <img style={{ position:"relative", right:"14vw", width:"15vw", display:"block"}} src={man} alt="pic" align="right"></img>
+      <Box
+        sx={{ padding: "3vw", height: "17.3vw", backgroundColor: "#BBE1FA" }}
+      >
+        <Grid container>
+        <Grid item xs={6}>
+          <p
+            style={{
+              fontSize: "3vw",
+              fontFamily: "Montserrat",
+              fontWeight: 700,
+              color: "#1B262C",
+            }}
+          >
+            E-cube
+          </p>
+          </Grid>
+          <Grid item xs={6}>
+          <img
+            style={{
+              position: "relative",
+              right: "14vw",
+              width: "15vw",
+              display: "block",
+            }}
+            src={man}
+            alt="pic"
+            align="right"
+          ></img>
+          </Grid>
+        </Grid>
       </Box>
     </div>
   );
