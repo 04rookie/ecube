d2086f65f5f5ff3c7856fb9ab8a7d54d0a4742af
atharva <atharva.ek@gmail.com>
2021-11-18 15:50:32 +0530

    added dotenv

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 app.use(express.json());
+require("dotenv").config();
 app.use(express.static(path.join(__dirname, "/app")))
 const port = 5000;
 app.listen(process.env.PORT || port, () => {
